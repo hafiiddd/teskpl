7c1435a190f85d4a5b87e1a1c9fee1e1f1bd094b
hafiiddd <hafid.akhyar0411@gmail.com>
2025-01-24 17:33:23 +0700

    add swagger

---
+++ tesKPL/Controllers/tokoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tesKPL.Controllers
+{
+    [Route("api/[controller]")]
+    public class tokoController : Controller
+
+    {
+        public static List<tokoOnline> dataToko = new List<tokoOnline>
+        {
+            new tokoOnline("hafid","musang",1,20000,"01-january-2023","bogor",Shipping_Type.delivered),
+            new tokoOnline("eddy","musang",1,20000,"01-january-2023","bogor",Shipping_Type.delivered)
+
+        };
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(dataToko);
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] tokoOnline value)
+        {
+            dataToko.Add(value);
+            return CreatedAtAction(nameof(Post), value);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] tokoOnline value)
+        {
+            tokoOnline findPesanan = dataToko.ElementAt(id);
+            findPesanan.Customer_name = value.Customer_name;
+            findPesanan.total_price = value.total_price;
+            findPesanan.product_name = value.product_name;
+            findPesanan.shipping_address = value.shipping_address;
+            findPesanan.quantity = value.quantity;
+            findPesanan.order_date = value.order_date;
+            return Ok(findPesanan);
+        }
+        [HttpGet("{id}")]
+        public tokoOnline Get(int id)
+        {
+            return dataToko[id];
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataToko.RemoveAt(id);
+        }
+    }
+}
---
+++ tesKPL/tokoOnline.cs
@@ -0,0 +1,35 @@
+﻿namespace tesKPL
+{
+    public enum Shipping_Type { pending = 1, processed = 2, shipped = 3, delivered = 4};
+    public class tokoOnline
+    {
+        private string v1;
+        private string v2;
+        private int v3;
+        private string v4;
+        private string v5;
+        private Shipping_Type delivered;
+
+        public String Customer_name { get; set; }
+        public String product_name { get; set; }
+        public int quantity { get; set; }
+        public int total_price { get; set; }
+        public String order_date { get; set; }
+        public String shipping_address { get; set; }
+       public Shipping_Type shipping_type { get; set; }
+
+        public tokoOnline(string Customer_name, string product_name, int quantity, int total_price, string order_date, string shipping_address, Shipping_Type shipping_type)
+        {
+            this.Customer_name = Customer_name;
+            this.product_name = product_name; 
+            this.quantity = quantity;
+            this.total_price = total_price;
+            this.order_date = order_date;
+            this.shipping_address = shipping_address;
+            this.shipping_type = shipping_type;
+
+        }
+
+
+    }
+}
